d3571d854e57ae9d77a986d76e041cb9f2811f04
k1LoW <k1lowxb@gmail.com>
2023-07-21 20:59:50 +0900

    Support s3:// for Amazon S3

--- remote.go
+++ remote.go
@@ -9,7 +9,11 @@ import (
 	"strings"
 	"time"
 
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go/service/s3/s3iface"
 	"github.com/google/go-github/v53/github"
+	"github.com/jszwec/s3fs"
 	"github.com/k1LoW/ghfs"
 	"github.com/k1LoW/go-github-client/v53/factory"
 )
@@ -20,15 +24,19 @@ var globalHTTPClient = &http.Client{
 
 var globalGitHubClient *github.Client
 
+var globalS3Client s3iface.S3API
+
 type clientSet struct {
 	httpClient   *http.Client
 	githubClient *github.Client
+	s3Client     s3iface.S3API
 }
 
 func newClientSet(opts []Option) *clientSet {
 	cs := &clientSet{
 		httpClient:   globalHTTPClient,
 		githubClient: globalGitHubClient,
+		s3Client:     globalS3Client,
 	}
 	for _, opt := range opts {
 		opt(cs)
@@ -52,6 +60,13 @@ func GitHubClient(c *github.Client) Option {
 	}
 }
 
+// S3Client set github.Client.
+func S3Client(c s3iface.S3API) Option {
+	return func(cs *clientSet) {
+		cs.s3Client = c
+	}
+}
+
 // Open remote file.
 func Open(raw string, opts ...Option) (io.ReadCloser, error) {
 	u, err := url.Parse(raw)
@@ -64,6 +79,8 @@ func Open(raw string, opts ...Option) (io.ReadCloser, error) {
 		return openHTTP(cs.httpClient, u)
 	case "github":
 		return openGitHub(cs.githubClient, raw)
+	case "s3":
+		return openS3(cs.s3Client, raw)
 	default:
 		p := strings.TrimPrefix(strings.TrimPrefix(raw, "file://"), "local://")
 		return os.Open(p)
@@ -109,5 +126,24 @@ func openGitHub(c *github.Client, raw string) (io.ReadCloser, error) {
 	if err != nil {
 		return nil, err
 	}
+	return fsys.Open(name)
+}
+
+func openS3(c s3iface.S3API, raw string) (io.ReadCloser, error) {
+	if c == nil && globalS3Client == nil {
+		sess, err := session.NewSession()
+		if err != nil {
+			return nil, err
+		}
+		c = s3.New(sess)
+		globalS3Client = c
+	}
+	splitted := strings.Split(strings.TrimPrefix(raw, "s3://"), "/")
+	if len(splitted) < 2 {
+		return nil, fmt.Errorf("invalid s3 path: %s", raw)
+	}
+	bucket := splitted[0]
+	name := strings.Join(splitted[1:], "/")
+	fsys := s3fs.New(c, bucket)
 	return fsys.Open(name)
 }
